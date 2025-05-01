ca87d4a13009ac13e89a6e02c3dd76c55caa744b
PUVVALA LAVANYA <145569662+P-lavanya16@users.noreply.github.com>
2025-05-01 12:51:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 // Setup CORS properly
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://hiringplatformcom.vercel.app'],  
+  origin: ['http://localhost:5173', 'https://hiringplatformcom.vercel.app/'],  
   credentials: true
 }));
 
