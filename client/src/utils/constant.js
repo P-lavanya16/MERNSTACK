f7ae534511a35618dd56c783d3370607e49d94df
PUVVALA LAVANYA <145569662+P-lavanya16@users.noreply.github.com>
2025-05-01 12:39:24 +0530

    Update constant.js

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,5 +1,6 @@
 //VITE_API_BASE_URL="http://localhost:8000/api/v1";
-const BASE_URL = "http://localhost:8000/api/v1";
+// const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://mernstack-backend-chi.vercel.app/api/v1";
 export const USER_API_END_POINT = `${BASE_URL}/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/job`;
 export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
